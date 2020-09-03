fa763078994512c04e8c202f020f50a4f9c852df
Doron Hafner <dh9707@gmail.com>
2020-09-03 13:56:21 +0300

    need to finish css and click img event

--- dist/main.js
+++ dist/main.js
@@ -5,11 +5,13 @@ $("#submit").on("click", function () {
     const input = $("#teamName").val().toLowerCase()
     $.get(`/api/teams/${input}`, function(data){
         renderRoster(data)
+        console.log(data);
     })
 })
 
 const renderRoster = (data) => {
     const newHTML = template({data});
+    console.log({data});
     $('#playersContainer').empty()
     $('#playersContainer').append(newHTML);
 }
